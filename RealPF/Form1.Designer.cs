5eb8e5065eacbd6a186829a93d09ca37d410d9e4
joanjones <78980205+joanjones@users.noreply.github.com>
2021-08-10 13:50:40 -0700

    real visula w map as of 8/10/2021

---
+++ RealPF/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace RealPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackImage = "C:\\Users\\lairi\\Downloads\\IMG-0806.JPG";
+            this.chart1.BorderlineColor = System.Drawing.SystemColors.Window;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.Maximum = 700D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX2.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.Maximum = 900D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.BackImageTransparentColor = System.Drawing.Color.Transparent;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.ShadowColor = System.Drawing.Color.Gray;
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.BorderColor = System.Drawing.Color.Transparent;
+            legend1.ForeColor = System.Drawing.Color.White;
+            legend1.Name = "Legend1";
+            legend1.TitleForeColor = System.Drawing.Color.Transparent;
+            legend1.TitleSeparatorColor = System.Drawing.Color.Transparent;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(124, 60);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Color = System.Drawing.Color.Aqua;
+            series1.Legend = "Legend1";
+            series1.Name = "Weight1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series2.Legend = "Legend1";
+            series2.Name = "Weight2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Color = System.Drawing.Color.Blue;
+            series3.Legend = "Legend1";
+            series3.Name = "Weight3";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            series4.Legend = "Legend1";
+            series4.Name = "Weight1_2";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Color = System.Drawing.Color.Fuchsia;
+            series5.Legend = "Legend1";
+            series5.Name = "Weight2_2";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Color = System.Drawing.Color.Purple;
+            series6.Legend = "Legend1";
+            series6.Name = "Weight3_2";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series7.Color = System.Drawing.Color.Black;
+            series7.Legend = "Legend1";
+            series7.MarkerSize = 7;
+            series7.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Triangle;
+            series7.Name = "Shark";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series8.Color = System.Drawing.Color.Black;
+            series8.Legend = "Legend1";
+            series8.MarkerSize = 7;
+            series8.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Triangle;
+            series8.Name = "Shark2";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series9.Color = System.Drawing.Color.Red;
+            series9.Legend = "Legend1";
+            series9.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Cross;
+            series9.Name = "AUV1";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series10.Color = System.Drawing.Color.Red;
+            series10.Legend = "Legend1";
+            series10.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Cross;
+            series10.Name = "AUV2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Size = new System.Drawing.Size(405, 279);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RealPF/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace RealPF
+{
+    public partial class Form1 : Form
+    {
+        private Thread cpuThread;
+        List<double> w1xList_pf1 = new List<double>();
+        List<double> w1yList_pf1 = new List<double>();
+        List<double> w2xList_pf1 = new List<double>();
+        List<double> w2yList_pf1 = new List<double>();
+        List<double> w3xList_pf1 = new List<double>();
+        List<double> w3yList_pf1 = new List<double>();
+
+        List<double> w1xList_pf2 = new List<double>();
+        List<double> w1yList_pf2 = new List<double>();
+        List<double> w2xList_pf2 = new List<double>();
+        List<double> w2yList_pf2 = new List<double>();
+        List<double> w3xList_pf2 = new List<double>();
+        List<double> w3yList_pf2 = new List<double>();
+
+        public int NUMBER_OF_SHARKS;
+        public int NUMBER_OF_ROBOTS;
+        public int NUMBER_OF_PARTICLEFILTERS;
+        public List<List<ParticleFilter>> particleFilterList = new List<List<ParticleFilter>>();
+        public List<List<Simulation>> simulationList = new List<List<Simulation>>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void create_real_range_list()
+        {
+            foreach (Shark s1 in MyGlobals.shark_list)
+            {
+                List<double> newList = new List<double>();
+                foreach (Robot r1 in MyGlobals.robot_list)
+                {
+                    newList.Add(20000);
+                }
+                MyGlobals.real_range_list.Add(newList);
+            }
+        }
+
+        public void create_and_initialize_sharks()
+        {
+            int SharkIndex = 0;
+            for (int i = 0; i < NUMBER_OF_SHARKS; ++i)
+            {
+                Shark s1 = new Shark();
+                s1.SHARKNUMBER = SharkIndex;
+                MyGlobals.shark_list.Add(s1);
+                SharkIndex += 1;
+            }
+        }
+        public void create_and_initialize_robots()
+        {
+            int RobotIndex = 0;
+            for (int i = 0; i < NUMBER_OF_ROBOTS; ++i)
+            {
+                Robot r1 = new Robot();
+                r1.ROBOTNUMBER = RobotIndex;
+                MyGlobals.robot_list.Add(r1);
+                RobotIndex += 1;
+            }
+        }
+
+        public void create_and_initialize_particle_filter()
+        {
+            for (int s = 0; s < NUMBER_OF_SHARKS; ++s)
+            {
+                List<ParticleFilter> partylist = new List<ParticleFilter>();
+                for (int r = 0; r < NUMBER_OF_ROBOTS; ++r)
+                {
+                    ParticleFilter p1 = new ParticleFilter();
+                    p1.create();
+                    partylist.Add(p1);
+                }
+                particleFilterList.Add(partylist);
+            }
+        }
+
+        public void create_simulation()
+        {
+            foreach (Shark s1 in MyGlobals.shark_list)
+            {
+                List<Simulation> currentSharkSim = new List<Simulation>();
+                foreach (Robot r1 in MyGlobals.robot_list)
+                {
+                    double rangeError = s1.calc_range_error(r1);
+                    Simulation sim = new Simulation(rangeError, s1, r1);
+                    currentSharkSim.Add(sim);
+
+                }
+                simulationList.Add(currentSharkSim);
+            }
+        }
+        //[[Sim1, Sim2], [Sim1, Sim2]]
+        public void update_simulation_list(int sharkNumber, int robotNumber)
+        {
+            simulationList[sharkNumber][robotNumber].rangeError = MyGlobals.shark_list[sharkNumber].calc_range_error(MyGlobals.robot_list[robotNumber]);
+        }
+        public void update_robots()
+        {
+            foreach (Robot r1 in MyGlobals.robot_list)
+            {
+                r1.update_robot_position();
+            }
+        }
+        public void update_sharks()
+        {
+            foreach (Shark s1 in MyGlobals.shark_list)
+            {
+                s1.update_shark();
+            }
+        }
+
+        public void update_pfs()
+        {
+            foreach (List<ParticleFilter> pflist in particleFilterList)
+            {
+                foreach (ParticleFilter pf1 in pflist)
+                {
+                    pf1.update();
+                }
+
+            }
+        }
+        public void clear_real_range_list()
+        {
+            List<List<double>> newList = new List<List<double>>();
+            MyGlobals.real_range_list = newList;
+            create_real_range_list();
+        }
+
+        public bool get_simulation()
+        {
+            if (MyGlobals.random_num.Next(0, 11) > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double calc_range_error(List<double> mean_particle, int sharkNum1)
+        {
+
+            double auvRange = Math.Sqrt(Math.Pow((mean_particle[1] - MyGlobals.shark_list[sharkNum1].Y), 2) + Math.Pow((mean_particle[0] - MyGlobals.shark_list[sharkNum1].X), 2));
+            return auvRange;
+        }
+        private void update_weight_lists(int index, ParticleFilter pf)
+        {
+            if (index == 0)
+            {
+                w1xList_pf1 =pf.w1_list_x;
+                w1yList_pf1 = pf.w1_list_y;
+                w2xList_pf1 = pf.w2_list_x;
+                w2yList_pf1 = pf.w2_list_y;
+                w3yList_pf1 = pf.w3_list_y;
+                w3xList_pf1 = pf.w3_list_x;
+            }
+            if (index == 1)
+            {
+                w1xList_pf2 = pf.w1_list_x;
+                w1yList_pf2 = pf.w1_list_y;
+                w2xList_pf2 = pf.w2_list_x;
+                w2yList_pf2 = pf.w2_list_y;
+                w3yList_pf2 = pf.w3_list_y;
+                w3xList_pf2 = pf.w3_list_x;
+            }
+
+        }
+
+        private void UpdateMap()
+        {
+
+            chart1.Series["Weight1"].Points.Clear();
+            chart1.Series["Weight2"].Points.Clear();
+            chart1.Series["Weight3"].Points.Clear();
+            chart1.Series["Weight1_2"].Points.Clear();
+            chart1.Series["Weight2_2"].Points.Clear();
+            chart1.Series["Weight3_2"].Points.Clear();
+            chart1.Series["Shark"].Points.Clear();
+            chart1.Series["Shark2"].Points.Clear();
+            chart1.Series["AUV1"].Points.Clear();
+            chart1.Series["AUV2"].Points.Clear();
+            //chart1.Series["AUV3"].Points.Clear();
+            chart1.Series["Shark"].Points.AddXY(MyGlobals.shark_list[0].X, MyGlobals.shark_list[0].Y);
+            chart1.Series["Shark2"].Points.AddXY(MyGlobals.shark_list[1].X, MyGlobals.shark_list[1].Y);
+            //add in one of pf2
+            for (int i = 0; i < w1xList_pf1.Count; ++i)
+            {
+                chart1.Series["Weight1"].Points.AddXY(w1xList_pf1[i], w1yList_pf1[i]);
+            }
+            for (int i = 0; i < w2xList_pf1.Count; ++i)
+            {
+                chart1.Series["Weight2"].Points.AddXY(w2xList_pf1[i], w2yList_pf1[i]);
+            }
+            for (int i = 0; i < w3xList_pf1.Count; ++i)
+            {
+                chart1.Series["Weight3"].Points.AddXY(w3xList_pf1[i], w3yList_pf1[i]);
+
+            }
+
+
+            for (int i = 0; i < w1xList_pf2.Count; ++i)
+            {
+                chart1.Series["Weight1_2"].Points.AddXY(w1xList_pf2[i], w1yList_pf2[i]);
+            }
+            for (int i = 0; i < w2xList_pf2.Count; ++i)
+            {
+                chart1.Series["Weight2_2"].Points.AddXY(w2xList_pf2[i], w2yList_pf2[i]);
+            }
+            for (int i = 0; i < w3xList_pf2.Count; ++i)
+            {
+                chart1.Series["Weight3_2"].Points.AddXY(w3xList_pf2[i], w3yList_pf2[i]);
+            }
+
+            //double hey = particle_filter.r1.robot_list_x.Count;
+            //double yes = particle_filter.r1.robot_list_y[0];
+            chart1.Series["AUV1"].Points.AddXY(MyGlobals.robot_list[0].X, MyGlobals.robot_list[0].Y);
+            chart1.Series["AUV2"].Points.AddXY(MyGlobals.robot_list[1].X, MyGlobals.robot_list[1].Y);
+            //chart1.Series["AUV3"].Points.AddXY(MyGlobals.robot_list[2].X, MyGlobals.robot_list[2].Y);
+
+        }
+        private void getParticleCoordinates()
+        { 
+            
+            NUMBER_OF_ROBOTS = 2;
+            NUMBER_OF_SHARKS = 2;
+            NUMBER_OF_PARTICLEFILTERS = NUMBER_OF_ROBOTS * NUMBER_OF_SHARKS;
+            create_and_initialize_sharks();
+            create_and_initialize_robots();
+            create_and_initialize_particle_filter();
+
+            
+            create_simulation();
+            create_real_range_list();
+            while (true)
+            {
+                update_sharks();
+                update_robots();
+                if (get_simulation())
+                {
+                    update_simulation_list(0, 0);
+                    update_simulation_list(0, 1);
+
+                    if (get_simulation())
+                    {
+                        update_simulation_list(1, 0);
+                        update_simulation_list(1, 1);
+
+                    }
+
+                }
+                clear_real_range_list();
+                foreach (List<Simulation> simlist in simulationList)
+                {
+                    foreach (Simulation sim in simlist)
+                    {
+                        sim.update_real_range_list();
+                    }
+                }
+                update_pfs();
+                int sharkNum = 0;
+                foreach (List<ParticleFilter> pflist in this.particleFilterList)
+                {
+                    foreach (ParticleFilter pf in pflist)
+                    {
+                        //pf.update();
+                        pf.update_weights(MyGlobals.real_range_list, sharkNum);
+                        pf.correct();
+
+                    }
+
+                    sharkNum += 1;
+                }
+                int sharkNum1 = 0;
+                foreach (List<ParticleFilter> pflist in this.particleFilterList)
+                {
+                    int robotNum = 0;
+                    foreach (ParticleFilter pf in pflist)
+                    {
+                        pf.weight_list_x();
+                        pf.weight_list_y();
+
+                        if (sharkNum1 == 0 & robotNum == 0)
+                        {
+                            int index = 0;
+                            update_weight_lists(index, pf);
+                        }
+                        else if (sharkNum1 == 1 & robotNum == 0)
+                        {
+                            int index = 1;
+                            update_weight_lists(index, pf);
+                        }
+                    }
+                    sharkNum1 += 1;
+                }
+
+
+                if (chart1.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateMap(); });
+                }
+                else
+                {
+                    //......
+                }
+
+
+                Thread.Sleep(100);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cpuThread = new Thread(new ThreadStart(this.getParticleCoordinates));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
